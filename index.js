e5e674e6185b02bc005dceea126674e12e5d892d
MaksymusPrime <grafdemaxo@gmail.com>
2023-09-08 00:20:57 +0300

    jsbase

---
+++ index.js
@@ -0,0 +1,90 @@
+let now_playing = document.querySelector('.now_playing');
+let track_art = document.querySelector('.track_art');
+let track_name = document.querySelector('.track_name');
+let track_artist = document.querySelector('.track_artist');
+
+let playpause_btn = document.querySelector('.playpause-track');
+let next_btn = document.querySelector('.next-track');
+let prev_btn = document.querySelector('.prev-track');
+
+let seek_slider = document.querySelector('.seek-slider');
+let volume_slider = document.querySelector('.volume-slider');
+let curr_time = document.querySelector('.current-time');
+let total_duration = document.querySelector('.total-duration');
+let wave = document.getElementById ('wave');
+let randomIcon = document.querySelector('.fa-random');
+let curr_track = document.createElement('audio');
+
+let track_index = 0;
+let isPlaying = false;
+let isRandom = false;
+let updateTimer = 0;
+
+const music_list = [
+    {
+        img: 'images/maroon.jpg',
+        name: 'Sugar',
+        artist: 'Maroon 5',
+        music: 'music/sugar.mp3'
+    },
+    {
+        img: 'images/maroon.jpg',
+        name: 'Sugar',
+        artist: 'Maroon 5',
+        music: 'music/sugar.mp3'
+    },
+    {
+        img: 'images/maroon.jpg',
+        name: 'Sugar',
+        artist: 'Maroon 5',
+        music: 'music/sugar.mp3'
+    },
+    {
+        img: 'images/maroon.jpg',
+        name: 'Sugar',
+        artist: 'Maroon 5',
+        music: 'music/sugar.mp3'
+    },
+    {
+        img: 'images/maroon.jpg',
+        name: 'Sugar',
+        artist: 'Maroon 5',
+        music: 'music/sugar.mp3'
+    }
+]
+
+loadTrack(track_index);
+
+function loadTrack(track_index) {
+    clearInterval(updateTimer);
+    reset();
+
+    curr_track.src = music_list[track_index].music;
+    curr_track.load();
+
+    track_art.style.backgroundImage = 'url(' + music_list[track_index].img + ')';
+    track_name.textContent = music_list[track_index].name;
+    track_artist.textContent = music_list[track_index].artist;
+    now_playing.textContent = 'Playing music' + (track_index + 1) + 'of' + music_list.length;
+
+    updateTimer = setInterval(setUpdate, 1000);
+    curr_track.addEventListener('ended', nextTrack);
+    random_bg_color();
+
+}
+
+function random_bg_color () {
+    let hex = ['0', '1', '2', '3','4', '5', '6', '7','8', '9', 'a', 'b','c', 'd', 'e'];
+    let a;
+
+    function populate (a) {
+        for (let i=0; i<6; i++) {
+            let x = Math.round(Math.random() *14);
+            let y = hex[x];
+            a += y;
+                }
+                return a;
+
+    }
+    
+}
